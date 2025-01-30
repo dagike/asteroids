edb9c18fd3b5340acfcc44a1919d30bc42189866
dagike <isaac20053@hotmail.com>
2025-01-29 23:52:09 -0500

    add: 60 fps limit

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ import pygame
 from constants import *
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while(True):
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           return
-      print("test")
+      screen.fill("black")
       pygame.display.flip()
+
+      dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
